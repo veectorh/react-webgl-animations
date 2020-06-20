1ea7ed08497cb0e78ee6573c0538c47102c6c34c
devCbenin <devCbenin@gmail.com>
2020-06-20 21:09:08 +0100

    created a small interactive tween

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import { TweenLite, Power3 } from 'gsap'
 import './App.css';
 
 function App() {
+
+  let circle = useRef(null)
+  let circleRed = useRef(null)
+  let circleBlue = useRef(null)
+
+  const [expanded, setExpanded] = useState(false)
+
+  const handleExpand = () => {
+    TweenLite.to(circleRed, 1, { width: 200, height: 200})
+
+  }
+
+  const handleShrink = () => {
+    TweenLite.to(circleRed, 1, { width: 75, height: 75})
+  }
+
+  const toggleSize = () => {
+    setExpanded(expanded => !expanded)
+    expanded ? handleExpand() : handleShrink()
+  }
+
+  useEffect(() => {
+    //staggerFrom used to iterate through a list of tweens
+    TweenLite.staggerFrom(
+      [circle, circleRed, circleBlue],// target
+      2, //duration
+      {opacity: 0, x: 200, ease: Power3.easeOut}, //tween variables, x=horizontal motion
+      .5 //delay per iteration
+    )
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        <div className='circle-container'>
+          <div ref={el => circle = el} className='circle'></div>
+          <div onClick={toggleSize} ref={el => circleRed = el} className='circle red'></div>
+          <div ref={el => circleBlue = el} className='circle blue'></div>
+        </div>
+      </div>
     </div>
   );
 }
